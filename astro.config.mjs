2faea952c53757709a7a7f688cda2bcec466d26b
Eric J. Humphrey <eric.humphrey@choice-options.com>
2025-03-25 22:31:19 -0400

    Trying to autodeploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://ejhumphrey.github.io',
-  base: '/'
+  site: 'https://ejhumphrey.ai'
 });
